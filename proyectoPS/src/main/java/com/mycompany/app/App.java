737c9a4ebdc5310cd18cdfb32bf9108da1b83a4b
Kike <kike.lopez@opendeusto.es>
2020-03-22 14:34:15 +0100

    commit

--- proyectoPS/src/main/java/com/mycompany/app/App.java
+++ proyectoPS/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!Eneko Otra vez" );
+        System.out.println( "eyeyey" );
     }
 }
