737fb88ef5f8a6395849c26712e3f850cf8e7cc6
alvarfez <alvar.fernandez@opendeusto.es>
2020-04-19 11:14:29 +0200

    He metido mas DTO y assemblers

--- src/main/java/es/deusto/spq/data/dto/UsuarioAssembler.java
+++ src/main/java/es/deusto/spq/data/dto/UsuarioAssembler.java
@@ -1,5 +1,24 @@
 package es.deusto.spq.data.dto;
 
+import es.deusto.spq.data.Usuario;
+
 public class UsuarioAssembler {
 
+	public UsuarioDTO assemble(Usuario usuario) {
+		
+		if(usuario == null) return null;
+		
+		UsuarioDTO usuarioDTO = new UsuarioDTO();
+		
+		usuarioDTO.setApellidos(usuario.getApellidos());
+		usuarioDTO.setEmail(usuario.getEmail());
+		usuarioDTO.setNickname(usuario.getNickname());
+		usuarioDTO.setNombre(usuario.getNombre());
+		usuarioDTO.setPw1(usuario.getPw1());
+		usuarioDTO.setPw2(usuario.getPw2());
+		usuarioDTO.setTienePiso(usuario.isTienePiso());
+		
+		return usuarioDTO;
+		
+	}
 }
--- src/main/java/es/deusto/spq/data/dto/UsuarioDTO.java
+++ src/main/java/es/deusto/spq/data/dto/UsuarioDTO.java
@@ -1,5 +1,83 @@
 package es.deusto.spq.data.dto;
 
-public class UsuarioDTO {
+import java.io.Serializable;
 
+public class UsuarioDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String nickname;  		//La usamos como primaryKey
+	
+	private String nombre;	
+	private String apellidos;		//Apellido1 + " " + Apellido2
+	
+	private String email;
+	
+	
+	private String pw1;				//TODO encriptar pw.
+	private String pw2;
+	
+	private boolean tienePiso;
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPw1() {
+		return pw1;
+	}
+
+	public void setPw1(String pw1) {
+		this.pw1 = pw1;
+	}
+
+	public String getPw2() {
+		return pw2;
+	}
+
+	public void setPw2(String pw2) {
+		this.pw2 = pw2;
+	}
+
+	public boolean isTienePiso() {
+		return tienePiso;
+	}
+
+	public void setTienePiso(boolean tienePiso) {
+		this.tienePiso = tienePiso;
+	}
+	
+	
+	
 }
