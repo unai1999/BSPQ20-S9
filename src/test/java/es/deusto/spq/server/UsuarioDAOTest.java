b4aaf431c8b2b78dd68a0990d30c3c7fd045178b
unai1999 <33113643+unai1999@users.noreply.github.com>
2020-05-08 20:42:52 +0200

    Borrar test repetido

--- src/test/java/es/deusto/spq/server/UsuarioDAOTest.java
+++ src/test/java/es/deusto/spq/server/UsuarioDAOTest.java
@@ -9,23 +9,6 @@ import es.deusto.spq.data.Usuario;
 
 public class UsuarioDAOTest {
 	
-	UsuarioDAO usuarioDAO;
-	Usuario u1;
-	
-	@Before
-	public void setUp() {
-		usuarioDAO = new UsuarioDAO();
-		u1 = new Usuario("aa", "bb", "cc", "dd", "ee");
-	}
-	@Test 
-	public void comprobarUsuarioDAO(){
-		usuarioDAO.guardar(u1);
-		//System.out.println(usuarioDAO.getUsuario(u1.getNickname()));
-		assertEquals(u1.getNombre(), usuarioDAO.getUsuario(u1.getNickname()).getNombre());
-		assertEquals(u1.getNombre(), usuarioDAO.getUsuarioFromEmail(u1.getEmail()).getNombre());
-		usuarioDAO.actaulizarDatosUsuario("aa", "cc", "dd", "ee", "ff");
-	}
-    
 	UsuarioDAO uDAO;
 	Usuario u;
 	
@@ -42,6 +25,7 @@ public class UsuarioDAOTest {
 	@Test
 	public void guardarTest() {
 		uDAO.guardar(u);
-		assertEquals(u, uDAO.getUsuarioFromEmail(u.getEmail()));
+		assertEquals(u.getNombre(), uDAO.getUsuarioFromEmail(u.getEmail()).getNombre());
+		assertEquals(u.getNombre(), uDAO.getUsuario(u.getNickname()).getNombre());
 	}
 }
