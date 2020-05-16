c20694d782cef30a4d62b99c6c0222148e464bc8
unai1999 <33113643+unai1999@users.noreply.github.com>
2020-05-16 20:17:50 +0200

    intento3

--- src/test/java/es/deusto/spq/data/PostTest.java
+++ src/test/java/es/deusto/spq/data/PostTest.java
@@ -22,15 +22,22 @@ public class PostTest {
 
         @Before
 	    public void setUp() {
+        	listaComentario = new ArrayList<Comentario>();
             post = new Post("Titulo", "Autor", "Contenido");
             c = new Comentario(1, "Autor", "Contenido");
+            //listaComentario.add(c);
             post.setAutor("Eneko");
             post.setContenido("aaa");
             post.setLikes(3);
+            //post.setComentarios(listaComentario);
             post.setTitulo("bbbb");
             c.setContenido("aaaa");
     	}
-        
+//        @Test
+//        public void setComentariosTest() {
+//        	assertTrue(post.getComentarios().size()> 0);
+//        }
+
         @Test
         public void setAutorTest() {
         	assertEquals("Eneko", post.getAutor());
@@ -43,6 +50,10 @@ public class PostTest {
             post.incLikes();
             assertEquals(likesP + 1, post.getLikes());
         }
-
+//        @Test
+//        public void addComentarioTest(){
+//            assertEquals(1, post.getComentarios().size());
+//            assertEquals(c, post.getComentarios().get(0));
+//        }
   
 }
