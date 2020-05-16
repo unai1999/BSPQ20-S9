33caecde53f5214778813725da2b27779948a374
unai1999 <33113643+unai1999@users.noreply.github.com>
2020-05-16 18:59:25 +0200

    Intento

--- src/test/java/es/deusto/spq/server/PagoServerTest.java
+++ src/test/java/es/deusto/spq/server/PagoServerTest.java
@@ -3,18 +3,6 @@
 //import org.junit.Before;
 //import org.junit.Test;
 //import org.mockito.Mock;
-//import org.mockito.Mockito;
-//
-//import es.deusto.spq.data.Pago;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.ArgumentMatchers.anyString;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.when;
-//
-//import javax.ws.rs.client.WebTarget;
-//import javax.ws.rs.core.Response;
 //
 //public class PagoServerTest {
 //	
@@ -29,12 +17,7 @@
 //	
 //	@Test
 //	public void realizarPagoTest() {
-//		df = mock(DAOFactory.class, Mockito.RETURNS_DEEP_STUBS);
-//		Pago p = new Pago();
-//		p.setEmail("ju@gmail.com"); p.setPrecio(300);
-//		Response r = server.realizarPago(p);
-//		assertEquals(200, r.getStatus());
-//		
+//		// TODO probar método del servidor con mockito
 //	}
 //
 //}
