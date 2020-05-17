1a81bca040842484f1cf4698dc3fc7097faa64a6
alvarfez <alvar.fernandez@opendeusto.es>
2020-05-17 11:36:13 +0200

    Test ComentariosDAO
    Test de las clases ComentarioPisoDAO y de la clase ComentarioPostDAO

---
+++ src/test/java/es/deusto/spq/server/ComentarioPostDAOTest.java
@@ -0,0 +1,51 @@
+package es.deusto.spq.server;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import es.deusto.spq.data.ComentarioPost;
+import es.deusto.spq.data.Post;
+
+public class ComentarioPostDAOTest {
+
+	
+	ComentarioPostDAO comentarioDAO;
+	Post post;
+	ComentarioPost comentarioPost;
+	List<ComentarioPost> comentarios;
+	String texto;
+	
+	@Before
+	public void setUp() {
+		comentarioDAO = new ComentarioPostDAO();
+		post = new Post();
+		post.setAutor("Eneko");
+        post.setContenido("aaa");
+        post.setLikes(3);
+        post.setTitulo("bbbb");
+  
+		comentarioPost = new ComentarioPost(post, "Prueba JTest");
+		
+		
+	}
+	
+	@Test
+	public void guardarTest() {
+		comentarioDAO.guardarComentario(comentarioPost);
+		
+		comentarios = comentarioDAO.getComentarios(post);
+		texto = "";
+		
+		for(ComentarioPost c : comentarios) {
+			if(c.getTexto().equals(comentarioPost.getTexto())) {
+				texto = c.getTexto();
+			}
+		}
+		
+		assertEquals(comentarioPost.getTexto(), texto);
+	}
+}
